7c0591790c26ccf37f89b6b76fbaafa1708802e2
Jitendra <jitendra.kumar@knoldus.com>
2023-03-28 11:41:19 +0530

    file modified

--- src/main/scala/com/knoldus/ExpandableIncreament.scala
+++ src/main/scala/com/knoldus/ExpandableIncreament.scala
@@ -1,9 +1,12 @@
 package com.knoldus
 
-object ExpandableIncreament extends App{
-  def increamentValue(number: Int, increamentBy: Int) = number + increamentBy
+object ExpandableIncreament extends App {
+  def increamentValue(number: Int, increamentBy: Int): Int = number + increamentBy
 
-  println(increamentValue(2,1))
-  println(increamentValue(2,2))
-  println(increamentValue(2,3))
+  println(increamentValue(2, 1))
+  println(increamentValue(2, 2))
+  println(increamentValue(2, 3))
+  println(increamentValue(2, 4))
+  println(increamentValue(2, 5))
+  println(increamentValue(2, 6))
 }
